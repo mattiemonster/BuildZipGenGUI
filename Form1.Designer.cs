cb3c89e6d2a3665db0720ba5edb439d2fae3212b
Mattie <mattiemonster1@hotmail.com>
2019-03-09 02:17:44 +0000

    UI Design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,13 @@
             this.folderLocBrowse = new System.Windows.Forms.Button();
             this.projectNameText = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.genWinCheck = new System.Windows.Forms.CheckBox();
+            this.genLinuxCheck = new System.Windows.Forms.CheckBox();
+            this.genMacCheck = new System.Windows.Forms.CheckBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.genWebGLCheck = new System.Windows.Forms.CheckBox();
+            this.genCustomCheck = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
@@ -88,11 +95,88 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Project name:";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "What should be be generated?";
+            // 
+            // genWinCheck
+            // 
+            this.genWinCheck.AutoSize = true;
+            this.genWinCheck.Location = new System.Drawing.Point(38, 141);
+            this.genWinCheck.Name = "genWinCheck";
+            this.genWinCheck.Size = new System.Drawing.Size(70, 17);
+            this.genWinCheck.TabIndex = 7;
+            this.genWinCheck.Text = "Windows";
+            this.genWinCheck.UseVisualStyleBackColor = true;
+            // 
+            // genLinuxCheck
+            // 
+            this.genLinuxCheck.AutoSize = true;
+            this.genLinuxCheck.Location = new System.Drawing.Point(38, 164);
+            this.genLinuxCheck.Name = "genLinuxCheck";
+            this.genLinuxCheck.Size = new System.Drawing.Size(51, 17);
+            this.genLinuxCheck.TabIndex = 8;
+            this.genLinuxCheck.Text = "Linux";
+            this.genLinuxCheck.UseVisualStyleBackColor = true;
+            // 
+            // genMacCheck
+            // 
+            this.genMacCheck.AutoSize = true;
+            this.genMacCheck.Location = new System.Drawing.Point(38, 187);
+            this.genMacCheck.Name = "genMacCheck";
+            this.genMacCheck.Size = new System.Drawing.Size(47, 17);
+            this.genMacCheck.TabIndex = 9;
+            this.genMacCheck.Text = "Mac";
+            this.genMacCheck.UseVisualStyleBackColor = true;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(231, 265);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 10;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            // 
+            // genWebGLCheck
+            // 
+            this.genWebGLCheck.AutoSize = true;
+            this.genWebGLCheck.Location = new System.Drawing.Point(38, 210);
+            this.genWebGLCheck.Name = "genWebGLCheck";
+            this.genWebGLCheck.Size = new System.Drawing.Size(63, 17);
+            this.genWebGLCheck.TabIndex = 11;
+            this.genWebGLCheck.Text = "WebGL";
+            this.genWebGLCheck.UseVisualStyleBackColor = true;
+            // 
+            // genCustomCheck
+            // 
+            this.genCustomCheck.AutoSize = true;
+            this.genCustomCheck.Enabled = false;
+            this.genCustomCheck.Location = new System.Drawing.Point(37, 233);
+            this.genCustomCheck.Name = "genCustomCheck";
+            this.genCustomCheck.Size = new System.Drawing.Size(92, 17);
+            this.genCustomCheck.TabIndex = 12;
+            this.genCustomCheck.Text = "Other/Custom";
+            this.genCustomCheck.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(314, 361);
+            this.ClientSize = new System.Drawing.Size(314, 295);
+            this.Controls.Add(this.genCustomCheck);
+            this.Controls.Add(this.genWebGLCheck);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.genMacCheck);
+            this.Controls.Add(this.genLinuxCheck);
+            this.Controls.Add(this.genWinCheck);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.projectNameText);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.folderLocBrowse);
@@ -117,6 +201,13 @@
         private System.Windows.Forms.Button folderLocBrowse;
         private System.Windows.Forms.TextBox projectNameText;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox genWinCheck;
+        private System.Windows.Forms.CheckBox genLinuxCheck;
+        private System.Windows.Forms.CheckBox genMacCheck;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.CheckBox genWebGLCheck;
+        private System.Windows.Forms.CheckBox genCustomCheck;
     }
 }
 
